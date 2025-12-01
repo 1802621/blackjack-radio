a5c58061d4811f3dc48dd17b7e3b04fee52a9462
1802621 <1802621@spsd.sk.ca>
2025-12-01 09:27:40 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+enum RadioMessage {
+    message1 = 49434
+}
+function Draw_Card () {
+    if (Score > 21) {
+        Bust = 1
+        basic.showString("BUST")
+    }
+}
+let Bust = 0
+let Score = 0
+let DRAWN_CARDS: number[] = []
+Score = 0
+Bust = 0
+basic.showString("Welcome")
+basic.forever(function () {
+    let Stand = 0
+    if (input.buttonIsPressed(Button.A) && (Bust != 1 && Stand != 1)) {
+        Draw_Card()
+    }
+})
